c3445e60285017997f6edc452a89db21aa07e63d
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-12-23 11:24:22 -0300

    agregar fechas inicio y termino

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let dias = document.getElementById("dias");
 
-dias.innerHTML = "4";
+// dias.innerHTML = "4";
 
+const fechaInicio = new Date('11/03/2026');
+const fechaFinal = new Date('11/03/2030');
+
+const diferenciaFecha = Math.abs(fechaFinal - fechaInicio);
+
+// 1000 ms * 60 s * 60 m * 24 h
+const diffDays = Math.floor(diferenciaFecha / (1000 * 60 * 60 * 24));
+
+dias.innerHTML = diffDays;
